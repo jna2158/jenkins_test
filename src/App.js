f0117ca0f6880ce59c2cb96f22646028902b9764
유지원 <jna215843@gmail.com>
2023-06-24 16:45:05 +0900

    build test2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - test
+          Learn React - test1
         </a>
       </header>
     </div>
