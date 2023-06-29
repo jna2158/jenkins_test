9a96791b6ee1c28042e4c7ab2f9cdb588bdc9dfe
유지원 <jna215843@gmail.com>
2023-06-29 21:42:44 +0900

    jenkins_test33

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - test32
+          Learn React - test33
         </a>
       </header>
     </div>
